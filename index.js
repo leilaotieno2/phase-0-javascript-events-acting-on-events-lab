90ec9aea625eab4e15aaf9075ecffa94fd6b5723
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-06 20:24:48 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+let DODGER = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let left = parseInt(DODGER.style.left.replace('px', ''), 10);
+
+  if (left > 0) {
+    DODGER.style.left = `${left - 4}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(DODGER.style.left.replace('px', ''), 10);
+
+  if (left < 360) {
+    DODGER.style.left = `${left + 4}px`;
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
